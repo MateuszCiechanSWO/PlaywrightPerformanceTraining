9dc14dabd1d17ad189a15eb28c7f6c02c7c64714
MateuszCiechanSWO <mateusz.ciechan@softwareone.com>
2024-07-03 14:07:03 +0200

    resource loading time

--- tests/navigationTimingAPI.spec.ts
+++ tests/navigationTimingAPI.spec.ts
@@ -4,7 +4,7 @@ import test from "./fixtures/fixture";
 test.describe('Get metrics from entering workspaces', () => {
 
     test.beforeEach(async ({ page, gdprConsent }) => {
-        await page.goto("https://kumo-test.com/workspaces");
+        await page.goto("https://kumo-test.com/");
         await gdprConsent.acceptButton.click();
     });
 
@@ -12,5 +12,11 @@ test.describe('Get metrics from entering workspaces', () => {
         const navigationTiming = JSON.parse(await page.evaluate(() => JSON.stringify(performance.getEntriesByType("navigation"))))[0].duration;
         console.log("Workspaces navigation duration: " + navigationTiming + " ms");
         await expect(navigationTiming).toBeLessThan(5000);
+    });
+
+    test('Resource loading time', async ({ page }) => {
+        await page.goto("https://kumo-test.com/workspaces");
+        const logoResourceTiming = (JSON.parse(await page.evaluate(() => JSON.stringify(window.performance.getEntriesByType('resource')))).find((element) => element.name.includes('css')));     
+        console.log(logoResourceTiming);
     });
 });
--- tests/pages/gdpr-consent-page.ts
+++ tests/pages/gdpr-consent-page.ts
@@ -1,10 +1,7 @@
 import { BasePage } from "./base-page";
-import { Locator, Page } from "@playwright/test";
+import { Locator } from "@playwright/test";
 
 export class GDPRConsent extends BasePage {
-  constructor(page: Page) {
-    super(page, "");
-  }
 
   get modalContainer(): Locator {
     return this.page.locator("div.ku-modal-container");
