00bdc8527e63dde74249dfa708a6ac389b252fe4
MateuszCiechanSWO <mateusz.ciechan@softwareone.com>
2024-07-08 11:21:28 +0200

    Cumulative layout shift

--- tests/timingAPI.spec.ts
+++ tests/timingAPI.spec.ts
@@ -52,4 +52,28 @@ test.describe('Get metrics from entering workspaces', () => {
         });
         console.log("Largest contentful paint was rendered in " + largestContentfulPaint.toFixed(2) + "ms");
     });
+
+    test('Layout instability score', async ({ page }) => 
+    {
+        await page.goto("https://kumo-test.com/workspaces");
+        
+        const cumulativeLayoutShift = await page.evaluate(() => {
+            return new Promise((resolve) => {
+                let CLS = 0;
+                new PerformanceObserver((l) => {
+                    const entries = l.getEntries();
+                    // the last entry is the largest contentful paint
+                    entries.forEach((entry) => {
+                        CLS += entry.value;
+                    })
+                    resolve(CLS);
+                })
+                .observe({
+                    type: 'layout-shift',
+                    buffered: true
+                });
+            });
+        });
+        console.log("Cumulative layout shift is " + cumulativeLayoutShift + "ms");
+    });
 });
